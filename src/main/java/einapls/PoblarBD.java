dca15c62302279fa8b9612c27e914f23677a1bd4
Javier Murillo <jmurillo93@gmail.com>
2016-05-26 00:22:14 +0200

    PoblarBD (no implementado)

--- src/main/java/einapls/PoblarBD.java
+++ src/main/java/einapls/PoblarBD.java
@@ -1,23 +1,20 @@
-package einapls.domain;
+package einapls;
 
-import einapls.domain.enumerations.TipoEdificio;
 import einapls.infrastructure.PoolConexiones;
 
 import java.sql.*;
 
 /**
- * Clase que se comunicará con e respositorio donde se encuentran los espacios
+ * Created by javmu on 26/05/2016.
  */
-public class RepositorioEspacios {
+public class PoblarBD {
 
-    //// TODO: 25/05/2016 TODO la implementacion, actualmente todo falseado
-    public static Espacio findEspacio (int id) {
+    public static void poblarDB() {
         Statement stmt = null;
         Connection con = PoolConexiones.getConex();
         try {
             stmt = con.createStatement();
-            PreparedStatement query = con.prepareStatement("SELECT * FROM adabyron_0 WHERE id = ?");
-            query.setInt(1, id);
+            PreparedStatement query = con.prepareStatement("SELECT * FROM adabyron_0");
 
             ResultSet rs = query.executeQuery();
             String id_centro = "";
@@ -31,17 +28,16 @@ public class RepositorioEspacios {
                 String area_gem = rs.getString("area_geom");
                 System.out.println("IDD: " + idd);
 
+                if (id_centro.startsWith("AULA") || id_centro.startsWith("LABORATORIO")) {
+
+                    // INSERTAR EN TABLA ESPACIO CON NUEVOS CAMPOS (OCUPACION, ETC).
+                }
+
             }
 
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    return null;
-
-    }
-
-    public static Espacio[] findEspacios (TipoEdificio tipoEdificio, int id) {
-        return null;
     }
 }
