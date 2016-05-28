64eb2f3548a355d93bfa76f6253524069f2ca8c1
Javier Murillo <jmurillo93@gmail.com>
2016-05-28 05:23:53 +0200

    Horario fix

--- src/main/java/einapls/application/SimuladorHorario.java
+++ src/main/java/einapls/application/SimuladorHorario.java
@@ -1,25 +1,10 @@
 package einapls.application;
 
-/**
- * Created by javmu on 28/05/2016.
- */
+import einapls.puertosYAdaptadores.SimuladorConectorHorario;
+
 public class SimuladorHorario {
 
     public static String getHorario() {
-        String horariosJSON = "{ \"8:00\": \"Fisica\"," +
-                "\"9:00\": \"Programacion\"," +
-                "\"10:00\": \"Laboratorio de Software\"," +
-                "\"11:00\": \"Ingenieria de Requisitos\"," +
-                "\"12:00\": \"Arquitectura Software\"," +
-                "\"13:00\": \"Videojuegos\"," +
-                "\"14:00\": \"Sistemas de la Informacion\"," +
-                "\"15:00\": \"Redes de Computadores\"," +
-                "\"16:00\": \"Proyecto Software\"," +
-                "\"17:00\": \"Ingenieria Web\"," +
-                "\"18:00\": \"Sistemas y Tecnologias Web\"," +
-                "\"19:00\": \"LIBRE\"," +
-                "\"20:00\": \"Sistemas Distribuidos\"," +
-                "\"21:00\": \"LIBRE\"}";
-        return horariosJSON;
+        return SimuladorConectorHorario.getHorario();
     }
 }
